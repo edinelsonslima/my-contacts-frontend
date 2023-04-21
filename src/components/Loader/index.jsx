cfbdfbeeb22b8f7b332ea4bdc4cd99e3e10e8c3d
edinelsonslima <edinelsonlima86@gmail.com>
2023-04-21 00:15:27 -0300

    Implements component spinner

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -2,11 +2,15 @@ import ReactDOM from 'react-dom';
 import propTypes from 'prop-types';
 
 import { Overlay } from './styles';
+import Spinner from '../Spinner';
 
 export default function Loader({ loading }) {
   if (!loading) return null;
+
   return ReactDOM.createPortal(
-    <Overlay><div className="loader" /></Overlay>,
+    <Overlay>
+      <Spinner size={90} />
+    </Overlay>,
     document.getElementById('loader-root'),
   );
 }
