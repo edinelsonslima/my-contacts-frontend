1a2927ead75c24e1a708ff3fde865ac2c6053df4
edinelsonslima <edinelsonlima86@gmail.com>
2023-04-21 00:15:56 -0300

    Request for create new contact

--- src/pages/NewContact/index.jsx
+++ src/pages/NewContact/index.jsx
@@ -1,12 +1,27 @@
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import contactsService from '../../services/contactsService';
 
 export default function NewContact() {
+  function handleSubmit(formData) {
+    const contact = {
+      name: formData.name,
+      email: formData.email,
+      phone: formData.phone,
+      category_id: formData.categoryId,
+    };
+
+    contactsService.createContact(contact)
+      .then(console.log)
+      .catch(() => alert('Ocorreu um erro ao cadastrar o contato. Tente novamente.'))
+      .finally();
+  }
+
   return (
     <>
       <PageHeader title="Novo Contato" />
 
-      <ContactForm buttonLabel="Cadastrar" />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
     </>
   );
 }
