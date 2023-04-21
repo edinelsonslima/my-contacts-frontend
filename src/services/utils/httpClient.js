368d64d92c78143cddb6fac3bcaf848d432b5a8f
edinelsonslima <edinelsonlima86@gmail.com>
2023-04-21 00:18:07 -0300

    Created method post Refactor for improved code

--- src/services/utils/httpClient.js
+++ src/services/utils/httpClient.js
@@ -6,21 +6,53 @@ class HttpClient {
     this.baseUrl = baseUrl;
   }
 
-  async get(path) {
-    const response = await fetch(`${this.baseUrl}${path}`);
+  get(path, options) {
+    return this.makeRequest(path, {
+      method: 'GET',
+      headers: options?.headers,
+    });
+  }
+
+  post(path, options) {
+    return this.makeRequest(path, {
+      method: 'POST',
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+
+  async makeRequest(path, options) {
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
+    if (options.headers) {
+      Object.entries(options.headers).forEach(([name, value]) => {
+        headers.append(name, value);
+      });
+    }
+
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      body: JSON.stringify(options.body),
+      method: options.method,
+      headers,
+    });
+
     const contentType = response.headers.get('content-type');
 
     if (!contentType?.includes('application/json')) {
       throw new APIError(response);
     }
 
-    const body = await response.json();
+    const responseBody = await response.json();
 
-    if (!response.ok) throw new APIError(response, body);
+    if (!response.ok) throw new APIError(response, responseBody);
 
-    await delay();
+    await delay(1500);
 
-    return body;
+    return responseBody;
   }
 }
 
