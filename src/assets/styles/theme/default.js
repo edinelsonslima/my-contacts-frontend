13ae4f30616d770d99023b095aae23924507bcd0
edinelsonslima <edinelsonlima86@gmail.com>
2023-05-23 00:34:39 -0300

    feat: added success color

--- src/assets/styles/theme/default.js
+++ src/assets/styles/theme/default.js
@@ -9,15 +9,18 @@ export default {
       main: '#5061FC',
       dark: '#3346F0',
     },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+      100: '#E5E5E5',
+    },
     danger: {
       light: '#F97171',
       main: '#FC5050',
       dark: '#F63131',
     },
-    gray: {
-      900: '#222222',
-      200: '#BCBCBC',
-      100: '#E5E5E5',
+    success: {
+      main: '#51CA73',
     },
   },
 };
